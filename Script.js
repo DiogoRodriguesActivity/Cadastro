8692719e608d282c296a0921c1bc6fe32149cc39
Diogo <DiogoRodriguesTwo@hotmail.com>
2026-09-10 14:58:56 +0000

    Atualização no JavaScript - Uso de AddEventListener, uso do toggle // Atualização no Css - Uso do :root (para criar variaveis padrão) e .close .block para o Modal

--- Script.js
+++ Script.js
@@ -4,20 +4,24 @@ const modal = document.getElementById("modal");
 const closeModalBtn = document.getElementById("closeModalBtn");
 const cadastroForm = document.getElementById("cadastroForm");
 
+openModalBtn.addEventListener("click", function(){
+  document.body.classList.toggle("block");
+})
 
-openModalBtn.onclick = function() {
-  modal.style.display = "block";
-}
+closeModalBtn.addEventListener("click", function(){
+  document.body.classList.toggle("close");
+});
 
 
-closeModalBtn.onclick = function() {
-  modal.style.display = "none";
-}
+// closeModalBtn.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 cadastroForm.onsubmit = function(event) {
   event.preventDefault();
 
   alert("Seu cadastro foi realizado!");
 
-  modal.style.display = "none";
+   document.body.classList.remove("block");
+   cadastroForm.reset();
 }
